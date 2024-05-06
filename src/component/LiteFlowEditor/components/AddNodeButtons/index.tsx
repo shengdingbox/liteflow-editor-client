61a816eaeb78de5ad710878b482d55bc02225a9e
wangshijiang <shijiangjiayou@163.com>
2024-05-06 17:11:10 +0800

    bugfix 调整边界新增节点情况

--- src/component/LiteFlowEditor/components/AddNodeButtons/index.tsx
+++ src/component/LiteFlowEditor/components/AddNodeButtons/index.tsx
@@ -4,19 +4,28 @@ import styles from './index.module.less';
 
 const AddNodeButtons: React.FC<any> = (props) => {
   const { node } = props;
+  const addNodeOnEdge = ({ clientX, clientY, targetEdges }: any) => {
+    if (targetEdges.length) {
+      node.model?.graph.trigger('graph:showContextPad', {
+        x: clientX,
+        y: clientY,
+        edge: targetEdges[0],
+      });
+    }
+  };
   const onPrepend = debounce(({ clientX, clientY }: any) => {
-    node.model?.graph.trigger('graph:showContextPad', {
-      x: clientX,
-      y: clientY,
-      edge: (node.model?.getIncomingEdges(node) || [])[0],
-    });
+    const targetEdges =
+      node.model?.getIncomingEdges(node) ||
+      node.model?.getOutgoingEdges(node) ||
+      [];
+    addNodeOnEdge({ clientX, clientY, targetEdges });
   }, 100);
   const onAppend = debounce(({ clientX, clientY }: any) => {
-    node.model?.graph.trigger('graph:showContextPad', {
-      x: clientX,
-      y: clientY,
-      edge: (node.model?.getOutgoingEdges(node) || [])[0],
-    });
+    const targetEdges =
+      node.model?.getOutgoingEdges(node) ||
+      node.model?.getIncomingEdges(node) ||
+      [];
+    addNodeOnEdge({ clientX, clientY, targetEdges });
   }, 100);
   return (
     <div className={classNames(styles.liteflowAddNodeButtons)}>
