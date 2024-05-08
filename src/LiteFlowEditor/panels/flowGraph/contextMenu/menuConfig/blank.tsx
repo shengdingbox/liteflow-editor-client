d12cb6c0dd4da8f2c1daad7be964f69fc377543e
wangshijiang <shijiangjiayou@163.com>
2024-05-08 16:18:09 +0800

    feature: 暴露右键菜单的全选选项

--- src/LiteFlowEditor/panels/flowGraph/contextMenu/menuConfig/blank.tsx
+++ src/LiteFlowEditor/panels/flowGraph/contextMenu/menuConfig/blank.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-
-import { Graph } from '@antv/x6';
 import { shortcuts } from '../../../../common/shortcuts';
 import { SnippetsOutlined } from '@ant-design/icons';
 
@@ -11,13 +9,13 @@ const blankMenuConfig = [
     icon: <SnippetsOutlined />,
     handler: shortcuts.selectAll.handler,
   },
-  {
-    key: 'paste',
-    title: '粘贴',
-    icon: <SnippetsOutlined />,
-    disabled: (flowGraph: Graph) => flowGraph.isClipboardEmpty(),
-    handler: shortcuts.paste.handler,
-  },
+  // {
+  //   key: 'paste',
+  //   title: '粘贴',
+  //   icon: <SnippetsOutlined />,
+  //   disabled: (flowGraph: Graph) => flowGraph.isClipboardEmpty(),
+  //   handler: shortcuts.paste.handler,
+  // },
 ];
 
 export default blankMenuConfig;
