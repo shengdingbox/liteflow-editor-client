5a5e042dd16812bf454ec44c49c22a02dd4c8a07
wangshijiang <shijiangjiayou@163.com>
2024-06-24 15:41:05 +0800

    refactor: 显示捕获异常Mock

--- src/LiteFlowEditor/panels/toolBar/widgets/mock.tsx
+++ src/LiteFlowEditor/panels/toolBar/widgets/mock.tsx
@@ -27,7 +27,7 @@ const Mock: React.FC<IProps> = (props) => {
 
   useEffect(() => {
     setTimeout(() => {
-      handleOnChange(ConditionTypeEnum.AND);
+      handleOnChange(ConditionTypeEnum.CATCH);
     }, 500);
   }, [flowGraph]);
 
